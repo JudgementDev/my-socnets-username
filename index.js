838615c2b75ae4b58c16d0e1a505de2cd27bf428
SWC S3 <60889521+SWCS3@users.noreply.github.com>
2022-01-24 23:19:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const TYPES			= require("./socnetTypes");
 const fetch 			= require("node-fetch-commonjs");
 const fs 			= require("fs");
 
-
-
 let app = express();
 
+//Here we use the same handler `draw`, declarated in handlers.js file 
 app.get("/username", draw);
 
 app.listen(process.env.PORT || 3000);

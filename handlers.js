b9598145fc10ff6ee7a5f85176574e72f5b1c193
JudgementDev <nedrocker227@gmail.com>
2022-01-28 17:36:56 +0200

    Added border for badges

--- handlers.js
+++ handlers.js
@@ -1,28 +1,32 @@
-const {createCanvas, loadImage} = require("canvas");
-const TYPES			= require("./socnetTypes");
-const fetch 			= require("node-fetch-commonjs");
+const {createCanvas, loadImage} 	= require("canvas");
+const TYPES								= require("./socnetTypes");
+const fetch 							= require("node-fetch-commonjs");
 
 
 /* Anyone who has worked with canvas knows what's going on here. For the rest, I'll explain
    Here the same badge is created that you see in my github profile (which with the username of discord and telegram)
 */
-let createImg = (txt, bg, color, type) => 
+let createImg = (txt, bg, color, type, borderColor) => 
 {
 	// here the base canvas is initialized, on which other elements will be hung later
 	let canvas = createCanvas(150, 50);
 	let ctx = canvas.getContext('2d');
 	
 	// Here the background of the canvas is defined and a rectangle of the same shape as the canvas is created.
-	ctx.fillStyle = bg;
-	ctx.fillRect(0, 0, 150, 50);
+	bg != color && bg != undefined ? ctx.fillStyle = bg : ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	
 	// This is the text, that is, the nickname in the social network, which is passed to the function parameters (txt).
-	// This is already text, that is, a nickname in the social network, which is passed to the function parameters (txt). 
 	// Color is also indicated through the parameters
 	ctx.font = 'monospace 50pt';
 	ctx.textAlign = 'center';
-	ctx.fillStyle = color; // TODO: check if the background colors are the same and
+	color != bg && color != undefined ? ctx.fillStyle = color : ctx.fillStyle = "black";
 	
+	// here we draw a frame for our badge
+	ctx.lineWidth = 3;
+	borderColor != bg && borderColor != undefined ? ctx.strokeStyle = borderColor : ctx.strokeStyle = borderColor;
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
 	// The icon of the social network is already looming here, which will be displayed 
 	// Pay attention to TYPES[type].icon - this is an image of a social network icon (or rather, the path to it), which is located in the social network object 
 	// (see file socnetsTypes.js)
@@ -48,25 +52,26 @@ let draw = async (req, res) =>
 	let username;
 	
 	// Everything should be simple here - we get data from the request
-        let bg = req.query.bg;
-        let color = req.query.color;
-        let type = req.query.type
+   let bg = req.query.bg;
+   let color = req.query.color;
+   let type = req.query.type;
+   let borderColor = req.query.borderColor;
 	
 	// Now that's more interesting. 
 	// Here we use some kind of asynchronous function that returns a value, namely the data received through the messenger API
-        let data = await getData(TYPES[type].url, TYPES[type].payload);
+   let data = await getData(TYPES[type].url, TYPES[type].payload);
 	
 	// and here is our "useless" variable, which will soon determine the fate (and value!!!)
-        if (type == "discord")
-        	username = data.username + "\n#" + data.discriminator;
-        else if (type == "telegram")
-        	username = "t.me/\n" + data.result.username
+   if (type == "discord")
+      username = data.username + "\n#" + data.discriminator;
+   else if (type == "telegram")
+      username = "t.me/\n" + data.result.username
 
 	// And finally, we display our image on the Internet.
 	// Do not forget about the headers, otherwise the server will swear that this is not a JSON object
 	// Then we create our badge, open the stream in which it will be stored and instantly transfer it to the response so that the user can admire our work :)
 	res.setHeader("Content-Type", "image/png");
-	createImg(username, bg, color, type).pngStream().pipe(res);	
+	createImg(username, bg, color, type, borderColor).pngStream().pipe(res);	
 }
 
 // This function, I think, on the shelf does not need to be disassembled. Here we fetch data via messenger API using fetch/
@@ -78,7 +83,7 @@ let getData = async (url, payload) => {
 
 	let data = await response.json();
 
-        return await data;
+   return await data;
 }
 
 module.exports = {draw, getData};
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const {getData, draw}		= require("./handlers");
-const express 			= require("express");
-const TYPES			= require("./socnetTypes");
-const fetch 			= require("node-fetch-commonjs");
-const fs 			= require("fs");
+const express 				= require("express");
+const TYPES					= require("./socnetTypes");
+const fetch 				= require("node-fetch-commonjs");
+const fs 					= require("fs");
 
 let app = express();
 
-//Here we use the same handler `draw`, declarated in handlers.js file 
+// Want to know what's on the home page? 
+// Basically nothing, so I redirect you to the github repository
+app.get("/", (req, res) => res.redirect("https://github.com/judgementdev/my-socnets-username"))
+
+// Here we use the same handler `draw`, declarated in handlers.js file 
 app.get("/username", draw);
 
 app.listen(process.env.PORT || 3000);
